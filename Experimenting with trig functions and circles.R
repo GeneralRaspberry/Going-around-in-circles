64bf4adf8085f8fb642f5c78a8f319b8986af58d
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-02 13:48:07 +0100

    Add files via upload
    Error: Mapping should be created with `aes()` or `aes_()`.

--- Experimenting with trig functions and circles.R
+++ Experimenting with trig functions and circles.R
@@ -1,9 +1,9 @@
 ##############################creating a unit circle######################################################
+library("ggplot2")
+
 
 a<-seq(from=0, to= (2*pi), by=pi/10000)
 
 b<-cos(a)^2+sin(a)^2
 
-plot(cos(a)^2,sin(a)^2)
-
-plot(cos(a),sin(a))
+ggplot(cos(a),sin(a))+geom_point()
